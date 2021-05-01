946ae0d419ff8f0efa3c8204932a8f2888b4f0d5
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-05-01 20:37:20 +0200

    Updated redux typing to make it harder to make common errors with forgetting unknkown types

--- src/store.js
+++ src/store.js
@@ -13,39 +13,56 @@ import type { ProjectDTO } from "./dtos.js"
 type State = {
 	projects: ?$ReadOnlyArray<Project>,
 }
+
+type ReduxInitAction = { type: "INIT" }
+type ProjectsLoadedAction = {
+	type: "PROJECTS_LOADED",
+	projects: $ReadOnlyArray<Project>,
+}
+
 type Action =
-	| {
-		type: "PROJECTS_LOADED",
-		projects: $ReadOnlyArray<Project>,
-	}
+	| ReduxInitAction
+	| ProjectsLoadedAction
+
+type AppThunkAction<A: Action, ReturnValue = Promise<void>> = ThunkAction<State, A, ReturnValue>
 
 type DispatchAction =
-	| ThunkAction<State, Action, Promise<void>>
+	| AppThunkAction<Action>
 	| Action
 
-export const fetchProjects: ThunkAction<State, Action, Promise<void>> = async(dispatch) => {
+function parseProject(dto: ProjectDTO): Project {
+	return new Project({
+		...dto,
+		tasks: dto.tasks?.map(x => new Task(x)),
+	})
+}
+
+export const fetchProjects: AppThunkAction<ProjectsLoadedAction> = async(dispatch) => {
 	const response = await fetch(`${SERVER_URL}/projects`)
 	const json: $ReadOnlyArray<ProjectDTO> = await response.json()
 	dispatch({
 		type: "PROJECTS_LOADED",
-		projects: json.map(x => new Project({
-			...x,
-			tasks: x.tasks?.map(x => new Task(x)),
-		})),
+		projects: json.map(parseProject),
 	})
 }
 
-function reducer(state: ?State, action: Action) : State {
+const defaultState: State = {
+	projects: null,
+}
+
+function reducer(state?: State = defaultState, action: Action) : State {
 	switch(action.type) {
 	case "PROJECTS_LOADED":
 		return {
 			...state,
 			projects: action.projects,
 		}
+	case "INIT":
+		// Don't do anything here. Redux does not always actually call it INIT
+		return state
 	default:
-		return {
-			projects: null,
-		}
+		(action: empty)
+		return state
 	}
 }
 
