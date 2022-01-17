144b84284e1126457a4221dc0aace2872aaa9736
Benjamin Horsleben <benjamin@fizkerinc.dk>
2022-01-17 01:39:07 +0100

    Wired up the cancel-buttons

--- src/routes/ShowProject.js
+++ src/routes/ShowProject.js
@@ -1,7 +1,7 @@
 // @flow strict
 
 import * as React from "react"
-import { useParams, Route, Routes } from "react-router-dom"
+import { useNavigate, useParams, Route, Routes } from "react-router-dom"
 
 import { useAppSelector as useSelector } from "../store"
 import { Page } from "./Page.js"
@@ -10,6 +10,7 @@ import { Task } from "../data.js"
 
 function EditTask({ project }) {
 	const { taskID } = useParams()
+	const navigate = useNavigate()
 
 	const task = taskID == null ? null : project.get("tasks")?.find(x => x.get("id") === taskID)
 
@@ -20,13 +21,14 @@ function EditTask({ project }) {
 	return <TaskEditView
 		task={task}
 		onSave={(task) => { console.log("saving", { task: task.toJS() }) }}
-		onCancel={() => { console.log("cancelling") }}
+		onCancel={() => { navigate("..") }}
 	/>
 }
 
 export function ShowProject() : React.Node {
 	const { projectID } = useParams()
 	const projects = useSelector(x => x.projects)
+	const navigate = useNavigate()
 
 	if(projectID == null) {
 		throw new Error("ShowProject must be used in a route with a :projectID parameter")
@@ -49,7 +51,7 @@ export function ShowProject() : React.Node {
 						project: project.get("id"),
 					})}
 					onSave={(task) => { console.log("saving", { task: task.toJS() }) }}
-					onCancel={() => console.log("cancelling") }
+					onCancel={() => { navigate(".") }}
 				/>
 			}/>
 			<Route path="/edit-task/:taskID" element={<EditTask project={project} />} />
